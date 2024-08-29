105ff7d2351c178e1409040badc7254d370a85f9
faaridzargar <zargarfaarid@gmail.com>
2024-08-29 13:09:15 +0530

    Old changes

---
+++ gemini/index.js
@@ -0,0 +1,43 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+const genAI = new GoogleGenerativeAI("AIzaSyDhzwmUI2doM0agRkEffZHLT03BUe9e5hk");
+
+async function run(prompt) {
+  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+  const result = await model.generateContent(prompt);
+  const response = await result.response;
+  const text = response.text();
+
+  return text;
+}
+
+app.get("/", async (req, res) => {
+  const prompt = "Introduce yourself";
+  const text = await run(prompt);
+  console.log(text);
+  res.render("index.ejs", {
+    text
+  });
+});
+
+app.post("/send", async (req, res) => {
+  const question = req.body["ask"];
+  const text = await run(question);
+  console.log(text);
+  res.render("index.ejs", {
+    text
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
